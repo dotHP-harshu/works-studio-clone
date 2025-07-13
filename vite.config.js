cb70b62e3ef0dfeadc889c3084faa49804942537
Harsh Prajapati <harshprajapaticoding@gmail.com>
2025-07-13 23:11:33 +0530

    added base to the config file

--- vite.config.js
+++ vite.config.js
@@ -5,4 +5,5 @@ import tailwindcss from "@tailwindcss/vite";
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [react(), tailwindcss()],
+  base: "/works-studio-clone/", // change this to your project's directory name
 });
